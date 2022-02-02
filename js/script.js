05482be082a43ea60f4c9afb75a5736448436bd9
Thomas Persbråten <83210455+Thomaspersbraaten@users.noreply.github.com>
2022-02-02 13:31:39 +0100

    1

--- js/script.js
+++ js/script.js
@@ -21,47 +21,32 @@ async function something(url, urlTwo) {
   // fetch posts
   const response = await fetch(url);
   const results = await response.json();
-  //   console.log(results);
-  //   console.log(results[0].author);
-  results.forEach(function (event) {
-    postContainer.innerHTML += `
-      <div>
-      <h2>
-      ${event.title.rendered}
-      </h2>
-      <div class="author-info"></div>
-      ${event.content.rendered}
-      </div>
-      `;
-  });
-  const authorInfo = document.querySelector(".author-info");
+
+  console.log(results);
 
   //  fetch author
   const userResponse = await fetch(urlTwo);
   const userResults = await userResponse.json();
-  let avatarArray = [];
+  //   let userArray = [];
 
-  for (let i = 0; i < userResults.length; i++) {
-    console.log(userResults[i]);
-
-    // authorInfo.innerHTML = `<img src="${test}">`;
-    // const authorImg = userResults[i].simple_local_avatar[96];
-    //     if (results[i].author === 2) {
-    //     }
-    //     if (results[i].author === 3) {
-    //     }
-    //     if (results[i].author === 4) {
-    //     }
-  }
-
-  //   for (let i = 0; i < results.length; i++) {
-  //     const authorInfo = document.querySelector(".author-info");
-  //     console.log(authorInfo);
-  //     if (results.author === i) {
-  //       authorInfo.innerHTML = `
-  //      ${userResults[i].name}`;
-  //     }
+  //   for (let i = 0; i < userResults.length; i++) {
+  //        userArray.push(userResults[i]);
   //   }
+
+  results.forEach(function (data) {
+    const authorInfo = document.querySelector(".author-info");
+    postContainer.innerHTML += `
+      <div>
+      <h2>
+      ${data.title.rendered}
+      </h2>
+      <div class="author-info"></div>
+      <p>
+      ${data.excerpt.rendered}
+      </p>
+      </div>
+      `;
+  });
 }
 
 something(postUrl, authorUrl);
