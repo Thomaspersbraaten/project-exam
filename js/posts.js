53b6ec827c65e06e655ad90842167a4f5684841e
Thomas Persbråten <83210455+Thomaspersbraaten@users.noreply.github.com>
2022-02-11 13:14:04 +0100

    11.02.2022 v2

--- js/posts.js
+++ js/posts.js
@@ -5,13 +5,20 @@ const loader = document.querySelector(".loader");
 const showMore = document.querySelector(".show-more-btn");
 
 const postUrl =
-  "https://tpbro.online/The-Environmentalist/wp-json/wp/v2/posts?per_page=12&_embed";
+  "https://tpbro.online/The-Environmentalist/wp-json/wp/v2/posts?per_page=100&_embed";
+
+var currentPostCount = 0;
+var morePostsCount = 9;
 
 async function getPosts(url) {
   try {
     const response = await fetch(url);
     const results = await response.json();
+    console.log(results);
     createHtml(results);
+    showMore.addEventListener("click", function () {
+      showMorePosts(results);
+    });
   } catch (error) {
     console.error(error);
     postsContainer.innerHTML = showErrorMessage(error);
@@ -32,28 +39,28 @@ function createHtml(results) {
 
   // Creates the latest posts //
 
-  latestPost.innerHTML += `
-    <a href="details.html?id=${results[0].id}" style="text-decoration:none" class="latest-post">
-    <div class="ribbon"> </div>
-      <div>
-       <h3>
-        ${results[0].title.rendered}
-       </h3>
-       <div class="author-info">
-       <p>Written by ${latestPostAuthor} </p>
-       <p>${date}</p>
-       </div>
-       <div class="post-intro">
-        ${results[0].excerpt.rendered}
-       </div>
-      </div>
-      <p class="link-text">Read More &rightarrow;</p>
-      </a>
-      `;
+  // latestPost.innerHTML += `
+  //   <a href="details.html?id=${results[0].id}" style="text-decoration:none" class="latest-post">
+  //   <div class="ribbon"> </div>
+  //     <div>
+  //      <h3>
+  //       ${results[0].title.rendered}
+  //      </h3>
+  //      <div class="author-info">
+  //      <p>Written by ${latestPostAuthor} </p>
+  //      <p>${date}</p>
+  //      </div>
+  //      <div class="post-intro">
+  //       ${results[0].excerpt.rendered}
+  //      </div>
+  //     </div>
+  //     <p class="link-text">Read More &rightarrow;</p>
+  //     </a>
+  //     `;
 
   // creates the rest of the posts up to a total of 10 included latest posts //
 
-  for (let i = 1; i < results.length; i++) {
+  for (let i = currentPostCount; i <= morePostsCount; i++) {
     const d = new Date(results[i].date);
     const year = d.getFullYear();
     const month = d.getMonth();
@@ -79,62 +86,68 @@ function createHtml(results) {
         <p class="link-text">Read More &rightarrow;</p>
         </a>
         `;
+    const post = document.querySelector(".post");
+
+    // console.log(currentPostCount);
   }
 
-  if (
-    postsContainer.childElementCount + latestPost.childElementCount <
-    results.length
-  ) {
-    morePostsInfo.innerHTML = `<p>There are more posts to show</p>`;
+  currentPostCount = currentPostCount + 10;
+  morePostsCount = morePostsCount + 10;
+  // console.log(currentPostCount);
+  // console.log(postsContainer.childElementCount);
+  // console.log(results.length);
+  // console.log(latestPost.childElementCount);
+
+  if (postsContainer.childElementCount === results.length) {
+    morePostsInfo.innerHTML = `<p>There are no more posts to show...</p>`;
   } else {
-    morePostsInfo.innerHTML = `<p> There are no more posts to show</p>`;
+    morePostsInfo.innerHTML = `<p>There are more posts to show</p>`;
   }
 }
 
-// const authorResponse = await fetch(urlTwo + results[i].author);
-// const authorResults = await authorResponse.json();
-// console.log(authorResults);
+function showMorePosts(results) {
+  if (postsContainer.childElementCount === results.length) {
+    return;
+  } else {
+    for (let i = currentPostCount; i < results.length; i++) {
+      const d = new Date(results[i].date);
+      const year = d.getFullYear();
+      const month = d.getMonth();
+      const day = d.getDate();
+      const date = day + "." + month + 1 + "." + year;
 
-// for (let j = 0; j < authorContainer.length; j++) {
-//   authorContainer[j].innerHTML += `
-//     ${authorResults[j].name}`;
-//   console.log(authorResults);
-// }
-
-// for (let j = 0; j < authorArray.length; j++) {
-//   if (authorArray[j].id === results[i].author) {
-//     console.log(2);
-//     authorContainer.innerHTML = `${authorArray}`;
-//   }
-// }
-
-// console.log(results[i].author);
-// const idMatch = (value) => value === results[i].author;
-// // console.log(idMatch);
-// function findAuthor(author, post) {
-//     return author === post.id;
-// }
-
-// const authorID = authorArray.findIndex(idMatch);
-// // console.log(authorArray.findIndex(idMatch));
-// // console.log(authorArray.id);
-// // if (authorArray.id === authorID) {
-// //   console.log("true");
-// // } else {
-// //   console.log("false");
-// // }
-
-// postsContainer.innerHTML += `
-// <a href="details.html?id=${results[0].id}" style="text-decoration:none" class="post">
-//   <div>
-//    <h2>
-//     ${results[0].title.rendered}
-//    </h2>
-//    <div class="author-info"></div>
-//    <div class="post-intro">
-//     ${results[0].excerpt.rendered}
-//    </div>
-//   </div>
-//   <p class="link-text">Read More &rightarrow;</p>
-//   </a>
-//   `;
+      const postAuthor = results[i]._embedded.author[0].name;
+      if (i === results.length) {
+        break;
+      } else {
+        postsContainer.innerHTML += `
+        <a href="details.html?id=${results[i].id}" style="text-decoration:none" class="post old-post">
+          <div>
+           <h3>
+            ${results[i].title.rendered}
+           </h3>
+           <div class="author-info">
+           <p>Written by ${postAuthor} </p>
+           <p>${date}</p>
+           </div>
+           <div class="post-intro">
+            ${results[i].excerpt.rendered}
+           </div>
+          </div>
+          <p class="link-text">Read More &rightarrow;</p>
+          </a>
+          `;
+      }
+    }
+  }
+  console.log(postsContainer.childElementCount);
+  console.log(results.length);
+  console.log(latestPost.childElementCount);
+  if (postsContainer.childElementCount === results.length) {
+    morePostsInfo.innerHTML = `<p>There are no more posts to show...</p>`;
+    showMore.classList.add("inactive-button");
+    showMore.classList.remove("active-button");
+  } else {
+    morePostsInfo.innerHTML = `<p>There are more posts to show</p>`;
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 const postContainer = document.querySelector(".posts-container");
 const navigationPages = document.querySelector(".navigation-pages");
 const totalPages = document.querySelector(".total-pages");
+const nextPageButton = document.querySelector(".next-page");
+const previousPageButton = document.querySelector(".previous-page");
+const currentPage = document.querySelector(".current-page");
+const loader = document.querySelector(".loader");
 
 const postUrl =
   "https://tpbro.online/The-Environmentalist/wp-json/wp/v2/posts?per_page=12&_embed";
+var pageIndex = 1;
+var pageValue = 3;
+
+var minimumPostValue = 0;
+var maximumPostValue = 3;
 
 async function getPosts(url) {
   try {
     const response = await fetch(url);
     const results = await response.json();
-    createHtml(results);
+    createHtml(results, 0);
+    nextPageButton.addEventListener("click", function () {
+      nextPage(results);
+    });
+    previousPageButton.addEventListener("click", function () {
+      previousPage(results);
+    });
   } catch (error) {
     postContainer.innerHTML = showErrorMessage(error);
   }
@@ -17,21 +32,27 @@ async function getPosts(url) {
 
 getPosts(postUrl);
 
-function createHtml(results) {
-  for (let i = 0; i < results.length; i++) {
+function createHtml(results, value) {
+  loader.style.display = "none";
+  currentPage.innerHTML = pageIndex;
+  for (let i = value; i < results.length; i++) {
     const postAuthor = results[i]._embedded.author[0].name;
     const data = results[i];
+
+    // Creates the date of the post
     const dateCreation = new Date(data.date);
     const year = dateCreation.getFullYear();
     const month = dateCreation.getMonth();
     const day = dateCreation.getDate();
     const date = day + "." + month + 1 + "." + year;
+    //
+
     const posts = results.length;
     const pageNumbers = posts / 4;
     const calculatedPageNumbers = Math.ceil(pageNumbers);
     totalPages.innerHTML = `${calculatedPageNumbers}`;
 
-    console.log(Math.ceil(pageNumbers));
+    // console.log(Math.ceil(pageNumbers));
     if (i === 4) {
       return;
     } else {
@@ -53,48 +74,108 @@ function createHtml(results) {
         </a>
         `;
     }
+
+    // pageValue++;
   }
 }
 
-// For each try
+function nextPage(results) {
+  const posts = results.length;
+  const pageNumbers = posts / 4;
+  const calculatedPageNumbers = Math.ceil(pageNumbers);
 
-//   results.forEach(function (data) {
-//     postContainer.innerHTML += `
-//     <a href="details.html?id=${data.id}" style="text-decoration:none">
-//       <div>
-//       <h2>
-//       ${data.title.rendered}
-//       </h2>
-//       <div class="author-info"></div>
-//       <div class="post-intro">
-//       ${data.excerpt.rendered}
-//       </div>
-//       </div>
-//       <p class="link-text">Read More</p>
-//       </a>
-//       `;
-//     const authorInfo = document.querySelector(".author-info");
-//     console.log(data.athor);
-//     console.log(data.author);
-//     if (data.author === 2) {
-//       authorInfo.innerhtml += `ok`;
-//     }
-//   });
+  if (pageIndex === calculatedPageNumbers) {
+    return;
+  } else {
+    pageIndex = pageIndex + 1;
+    currentPage.innerHTML = pageIndex;
+    postContainer.innerHTML = "";
+    pageValue = pageValue + 1;
 
-// for loop try
+    for (let i = pageValue; i < results.length; i++) {
+      const data = results[i];
+      const postAuthor = results[i]._embedded.author[0].name;
 
-// postContainer.innerHTML += `
-// <a href="details.html?id=${results[i].id}" style="text-decoration:none">
-//   <div>
-//   <h2>
-//   ${results[i].title.rendered}
-//   </h2>
-//   <div class="author-info">
-//   <p>${author.name}</p>
-//   <p>${date} </p></div>
-//   <div class="post-intro">
-//   ${results[i].excerpt.rendered}
-//   </div>
-//   </div>
-//   <p class="link-text">Read More</p>
-//   </a>
+      // Creates the date of the post
+      const dateCreation = new Date(data.date);
+      const year = dateCreation.getFullYear();
+      const month = dateCreation.getMonth();
+      const day = dateCreation.getDate();
+      const date = day + "." + month + 1 + "." + year;
+      //
+
+      if (i === pageValue + 4) {
+        break;
+      } else {
+        postContainer.innerHTML += `
+        <a href="details.html?id=${results[i].id}" style="text-decoration:none" class="post old-post">
+          <div>
+           <h3>
+            ${results[i].title.rendered}
+           </h3>
+           <div class="author-info">
+           <p>Written by ${postAuthor} </p>
+           <p>${date}</p>
+           </div>
+           <div class="post-intro">
+            ${results[i].excerpt.rendered}
+           </div>
+          </div>
+          <p class="link-text">Read More &rightarrow;</p>
+          </a>
+          `;
+      }
+    }
+    pageValue = pageValue + 4;
+    // console.log(pageValue);
+  }
+}
+
+function previousPage(results) {
+  pageValue = pageValue - 4;
+  if (pageIndex === 1) {
+    return;
+  } else {
+    pageIndex = pageIndex - 1;
+    currentPage.innerHTML = pageIndex;
+    postContainer.innerHTML = "";
+    console.log(pageValue);
+
+    for (let i = pageValue; i < results.length; i--) {
+      const data = results[i];
+      const postAuthor = results[i]._embedded.author[0].name;
+      console.log(i);
+
+      // Creates the date of the post
+      const dateCreation = new Date(data.date);
+      const year = dateCreation.getFullYear();
+      const month = dateCreation.getMonth();
+      const day = dateCreation.getDate();
+      const date = day + "." + month + 1 + "." + year;
+      //
+      if (pageValue === pageValue - 4) {
+        break;
+      } else {
+        postContainer.innerHTML = `
+        <a href="details.html?id=${results[i].id}" style="text-decoration:none" class="post old-post">
+          <div>
+           <h3>
+            ${results[i].title.rendered}
+           </h3>
+           <div class="author-info">
+           <p>Written by ${postAuthor} </p>
+           <p>${date}</p>
+           </div>
+           <div class="post-intro">
+            ${results[i].excerpt.rendered}
+           </div>
+          </div>
+          <p class="link-text">Read More &rightarrow;</p>
+          </a>
+          `;
+      }
+    }
+    pageValue = pageValue - 4;
+    console.log(pageValue);
+  }
+}
